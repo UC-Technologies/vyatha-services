1780d84d160a55446b5a6c35c2d2bc3623f224f1
krishnam <maheshwarikrishnam123@gmail.com>
2023-09-09 17:17:06 +0530

    ADD email validator that only validate NITS email IDs

---
+++ src/utils/EmailValidation.js
@@ -0,0 +1,8 @@
+const emailValidator = (req, res, next) => {
+    const email = req.body.emailID
+    if(!(/^[a-zA-Z0-9._-]+@[a-z]+\.nits\.ac\.in$/.test(email))) {
+        res.status(403).json({ error: "Not a valid email id" });
+    } else next();
+}
+
+module.exports = emailValidator
