14c84368e9e240a6d876452a409e946f124e6ea1
krishnam <maheshwarikrishnam123@gmail.com>
2023-09-11 22:52:00 +0530

    Changed response status code to 400

--- src/utils/EmailValidation.js
+++ src/utils/EmailValidation.js
@@ -1,7 +1,7 @@
 const emailValidator = (req, res, next) => {
     const email = req.body.emailID
     if(!(/^[a-zA-Z0-9._-]+@[a-z]+\.nits\.ac\.in$/.test(email))) {
-        res.status(403).json({ error: "Not a valid email id" });
+        res.status(400).json({ error: "Not a valid email id" });
     } else next();
 }
 
