f205a2d8f7f9387a23e9ce4a930668699d739e20
krishnam <maheshwarikrishnam123@gmail.com>
2023-09-11 23:25:31 +0530

    Change response status code to 400, now using object deconstructing and if else block

--- src/utils/PasswordValidation.js
+++ src/utils/PasswordValidation.js
@@ -1,23 +1,15 @@
 const passwordValidator = (req, res, next) => {
-    const password = req.body.password
-    const confirmPassword = req.body.confirmPassword
-    if (!(/\s/.test(password)||/\s/.test(confirmPassword))) {
-        if(password !== confirmPassword) res.status(403).json({ error: "Password must be same" });
-        else {
-            if(password.length < 8) res.status(403).json({ error: 'Password must be atleast 8 characters' });
-            else {
-                const lowerCase = /[a-z]/.test(password),
-                    upperCase = /[A-Z]/.test(password),
-                    numbers = /\d/.test(password),
-                    specialChar = /[`!@#$%^&*()_+=|:;<>,./?"'{}]/.test(password);
-                if(!(lowerCase && upperCase && numbers && specialChar))
-                    res.status(403).json({ error: 'Password must contain one uppercase, lowercas, digit and special character' });
-                else next();
-            }
-        }
-    } else {
-        res.status(403).json({ error: 'No space allowed in password' });
-    }
+    const { password, confirmPassword } = req.body;
+    const lowerCase = /[a-z]/.test(password),
+        upperCase = /[A-Z]/.test(password),
+        numbers = /\d/.test(password),
+        specialChar = /[`!@#$%^&*()_+=|:;<>,./?"'{}]/.test(password);
+    if (/\s/.test(password)||/\s/.test(confirmPassword)) res.status(400).json({ error: 'No space allowed in password' });
+    else if(password !== confirmPassword) res.status(400).json({ error: "Password must be same" });
+    else if(password.length < 8) res.status(400).json({ error: 'Password must be atleast 8 characters' });
+    else if(!(lowerCase && upperCase && numbers && specialChar))
+        res.status(400).json({ error: 'Password must contain one uppercase, lowercas, digit and special character' });
+    else next();
 }
 
 module.exports = passwordValidator
